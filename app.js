83db98c2a5fff2212270ace93b380e3dd27d263d
ifaxec <xecfahim.ahmed@gmail.com>
2023-02-26 21:26:02 +0000

    updated app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
-const { data } = require("./data.json");
+const data = require("./data.json");
 const { projects } = data;
+
+//console.log(projects);
+//console.log(projects[0].image_urls[0]);
+
 
 app.set("view engine", "pug");
 
@@ -16,12 +20,15 @@ app.get("/about", (req, res) => {
     res.render("about");
 });
 
-// app.get("/projects/:id", (req, res) => {
+app.get("/project/:id", (req, res) => {
+    const projectId = req.params.id;
+    const projectData = data.projects.find((project) => { project.id === +projectId });
 
-
-// });
-
-
+    if (projectData) {
+        req.locals.project = projectData;
+        res.render("project");
+    }
+});
 
 
 app.listen(3000, () => {
